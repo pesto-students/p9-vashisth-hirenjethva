98040e8427ed78338dea76eaf83f7656e97220e9
hirenjethva <hiren.j@somaiya.edu>
2023-02-21 23:43:25 +0530

    update code as per monu sir

--- Week-4/excersie1.js
+++ Week-4/excersie1.js
@@ -1,5 +1,27 @@
 
+//Updated code
+function getNumber(resolveTime, rejectTime){
+    let randomNum = parseInt(Math.random()*100);
+    console.log(randomNum);
 
+    if (randomNum % 5 == 0){
+        // Thenable object
+        return {
+            then(){
+                setTimeout(() => console.log(`custom promise has been REJECTED after ${rejectTime}ms`), rejectTime)
+            }
+        }
+    }
+    return {
+        // Thenable object
+        then(){
+            setTimeout(() => console.log(`custom promise has been RESOLVED after ${resolveTime}ms`), resolveTime)
+        }
+    }
+}
+
+getNumber(1000, 2000).then();
+/*
 function displayLog(some) {
     if(Number(some) % 5 == 0){
         setTimeout(() => console.log("Reject the promise " + some),2000);
@@ -14,7 +36,7 @@ function getNumber(mycallback){
 }
 
 getNumber(displayLog);
-
+*/
 
 
 
